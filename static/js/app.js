cd229aef6097c5a482b9fb95e9382d50c8d3bee7
Mav Sanchez <maverickssanchez@gmail.com>
2020-03-17 22:53:40 -0700

    Finalized bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,68 +1,25 @@
-
-// id: "940"
-// otu_ids: (80)[1167, 2859, 482, 2264, 41, 1189, 352, 189, 2318, 1977, 3450, 874, 1959, 2191, 1950, 2077, 2275, 944, 2184, 2244, 2024, 2419, 2811, 165, 2782, 2247, 2011, 2396, 830, 2964, 1795, 2722, 307, 2178, 2908, 1193, 2167, 1208, 2039, 1274, 2739, 2737, 1314, 1962, 2186, 2335, 2936, 907, 833, 2483, 2475, 2491, 2291, 159, 2571, 2350, 2342, 2546, 725, 170, 1505, 513, 259, 1169, 258, 1232, 1497, 1498, 1503, 412, 2235, 1960, 1968, 121, 2065, 340, 2110, 2188, 357, 342]
-// sample_values: (80)[163, 126, 113, 78, 71, 51, 50, 47, 40, 40, 37, 36, 30, 28, 25, 23, 22, 19, 19, 14, 13, 13, 13, 12, 12, 11, 11, 11, 10, 10, 10, 8, 7, 7, 7, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
-// otu_labels: (80)["Bacteria;Bacteroidetes;Bacteroidia;Bactero
 let subject_id = d3.select("#selDataset");
 let metadata_id = d3.select("#sample-metadata");
+let bubble_id = d3.select("#bubble");
 let json_raw = {};
 
-// function init(){
-//     let data = [
-//         {
-//             x: [1, 2, 3, 4, 5],
-//             y: [1, 2, 4, 8, 16]
-//         }
-//     ];
-//     Plotly.newPlot("bar", data);
-// }
-
-function set_options(html_obj, lst) {
-    lst_dist = new Set(lst);
-    lst_dist.forEach((opt) => {
-        let new_option = html_obj.append("option").text(opt.toUpperCase());
-        new_option.attr("value", opt);
-    });
-}
-
-function optionChanged(idNum){
-    let samples_data = json_raw.samples;
+function render_meta(idNum) {
     let metadata_data = json_raw.metadata;
-    console.log(metadata_data);
-    let selectedSample = samples_data.filter((sample) => { if (parseInt(sample.id) == parseInt(idNum)) return true; })[0];
     let selectedMeta = metadata_data.filter((meta) => { if (parseInt(meta.id) == parseInt(idNum)) return true; })[0];
-    // id: 940
-    // ethnicity: "Caucasian"
-    // gender: "F"
-    // age: 24
-    // location: "Beaufort/NC"
-    // bbtype: "I"
-    // wfreq: 2
-    // let htmlMeta = "<ul style='list-style-type: none'>"
+    // Put metadata information to DOM
     let htmlMeta = ""
     for (let [key, value] of Object.entries(selectedMeta)) {
-        // htmlMeta+= "<li>"+key+" : "+value+"</li>";
-        htmlMeta+= key+" : "+value+"<br/>";
+        htmlMeta += key + " : " + value + "<br/>";
     }
-
     metadata_id.html(htmlMeta);
-    // (selectedMeta.ethnicity);
+}
 
-    let mapped = []
-    for (i = 0; i < selectedSample.sample_values.length;i++){
-        let temp_d = {};
-        temp_d.otu_id = selectedSample.otu_ids[i];
-        temp_d.sample_values = selectedSample.sample_values[i];
-        temp_d.otu_labels = selectedSample.otu_labels[i];
-        mapped.push(temp_d);
-    }
-    
-    mapped = mapped.sort((a, b) => parseInt(b.sample_values) - parseInt(a.sample_values));
-    mapped = mapped.slice(0, 10);
-    mapped = mapped.reverse();
+function render_barchart(mapped, idNum) {
+
+    // Create and plot the top 10
     let trace = {
         x: mapped.map(row => row.sample_values),
-        y: mapped.map(row => "OTU "+row.otu_id),
+        y: mapped.map(row => "OTU " + row.otu_id),
         text: mapped.map(row => row.otu_labels),
         name: idNum,
         type: "bar",
@@ -70,22 +27,130 @@ function optionChanged(idNum){
     }
     let chart_data = [trace];
     let layout = {
-        title: "Sample ID "+idNum+" Results",
-        margin: {
-            l: 100,
-            r: 100,
-            t: 100,
-            b: 100
-        }
+        title: "Sample ID " + idNum + " Results",
+        margin: { l: 100, r: 100, t: 100, b: 100 }
     }
-
     Plotly.newPlot("bar", chart_data, layout);
+}
 
+function render_bubblechart(mapped, idNum) {
+    bubble_id.html("");
+
+    // Use otu_ids for the x values.
+    // Use sample_values for the y values.
+    // Use sample_values for the marker size.
+    // Use otu_ids for the marker colors.
+    // Use otu_labels for the text values.
+
+    let otus = mapped.map(row => row.otu_id);
+    let values = mapped.map(row => row.sample_values = +row.sample_values );
+    let labels = mapped.map(row => row.otu_labels);
+
+    let svgWidth = 500;
+    let svgHeight = 300;
+
+    // Define the chart's margins as an object
+    let chartMargin = {
+        top: 30,
+        right: 30,
+        bottom: 30,
+        left: 30
+    };
+
+    // Define dimensions of the chart area
+    let chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+    let chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+    let svg = d3.select('#bubble')
+        .append('svg')
+        .attr("width", svgWidth)
+        .attr("height", svgHeight);
+
+    let chartGroup = svg.append("g")
+        .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+    let xScale = d3.scaleBand()
+        .domain(otus)
+        .range([0, chartWidth]);
+
+    let yScale = d3.scaleLinear()
+        .domain([d3.min(values), d3.max(values)])
+        .range([chartHeight, 0]);
+    
+    let rScale = d3.scaleLinear()
+        .domain([d3.min(values), d3.max(values)])
+        .range([10, chartMargin.top]);
+
+    let opacity = d3.scaleLinear()
+        .domain([d3.min(values), d3.max(values)])
+        .range([1, .5]);
+
+    let colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+    let yAxis = d3.axisLeft(yScale);
+    let xAxis = d3.axisBottom(xScale);
+
+    chartGroup.append('g')
+        .attr('transform', `translate(0, ${chartHeight})`)
+        .call(xAxis);
+
+    chartGroup.append('g')
+        .call(yAxis);
+
+    chartGroup.selectAll("circle")
+        .data(values)
+        .enter()
+        .insert("circle")
+        .attr("cx", (data, index) => xScale(otus[index]))
+        .attr("cy", data => yScale(data))
+        .attr("opacity", data => opacity(data))
+        .attr("r", data => { return `${rScale(data)}`})
+        .style("fill", (data, index) => colorScale(otus[index]))
+        .append("title")
+        .text((data, index) => `${data}\n${labels[index]}`);
 
 }
 
+function optionChanged(idNum) {
+    let samples_data = json_raw.samples;
+
+    let selectedSample = samples_data.filter((sample) => { if (parseInt(sample.id) == parseInt(idNum)) return true; })[0];
+
+    //Build a selected samples array of JSON objects
+    let mapped = [];
+    for (i = 0; i < selectedSample.sample_values.length; i++) {
+        mapped.push({
+            otu_id: selectedSample.otu_ids[i],
+            sample_values: selectedSample.sample_values[i],
+            otu_labels: selectedSample.otu_labels[i]
+        });
+    }
+
+    // Slicing, sorting, reversing the returned samples
+    mapped = mapped.sort((a, b) => parseInt(b.sample_values) - parseInt(a.sample_values));
+    mapped = mapped.slice(0, 10);
+    mapped = mapped.reverse();
+    
+    console.log(selectedSample.sample_values.length)
+    render_meta(idNum);
+    render_barchart(mapped, idNum);
+    
+    mapped = mapped.sort((a, b) => parseInt(a.otu_id) - parseInt(b.otu_id));
+    console.log(mapped);
+    render_bubblechart(mapped, idNum);
+
+}
+
+// Initialize
 d3.json("samples.json").then((data) => {
     json_raw = data;
-    set_options(subject_id, json_raw.names.sort( (a,b) => a-b ));
-    // init();
+
+    subject_id.selectAll("option")
+        .data(json_raw.names)
+        .enter()
+        .append('option')
+        .text(data => data)
+        .attr('value', data => data);
+
+    optionChanged(Math.min(parseInt(json_raw.names)));
 });
